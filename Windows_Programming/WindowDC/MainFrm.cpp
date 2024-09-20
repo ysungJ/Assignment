f71049785abd5d4e2ea82d3ec63997c4a103508b
Yu-Sung <120106925+ysungJ@users.noreply.github.com>
2024-09-20 11:50:34 +0900

    WindowDC

---
+++ Windows_Programming/WindowDC/MainFrm.cpp
@@ -0,0 +1,116 @@
+﻿
+// MainFrm.cpp: CMainFrame 클래스의 구현
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "WindowDC.h"
+
+#include "MainFrm.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+// CMainFrame
+
+IMPLEMENT_DYNAMIC(CMainFrame, CFrameWnd)
+
+BEGIN_MESSAGE_MAP(CMainFrame, CFrameWnd)
+	ON_WM_CREATE()
+	ON_WM_SETFOCUS()
+	ON_WM_NCPAINT()
+END_MESSAGE_MAP()
+
+// CMainFrame 생성/소멸
+
+CMainFrame::CMainFrame() noexcept
+{
+	// TODO: 여기에 멤버 초기화 코드를 추가합니다.
+}
+
+CMainFrame::~CMainFrame()
+{
+}
+
+int CMainFrame::OnCreate(LPCREATESTRUCT lpCreateStruct)
+{
+	if (CFrameWnd::OnCreate(lpCreateStruct) == -1)
+		return -1;
+
+	// 프레임의 클라이언트 영역을 차지하는 뷰를 만듭니다.
+	if (!m_wndView.Create(nullptr, nullptr, AFX_WS_DEFAULT_VIEW,
+		CRect(0, 0, 0, 0), this, AFX_IDW_PANE_FIRST, nullptr))
+	{
+		TRACE0("뷰 창을 만들지 못했습니다.\n");
+		return -1;
+	}
+	return 0;
+}
+
+BOOL CMainFrame::PreCreateWindow(CREATESTRUCT& cs)
+{
+	if( !CFrameWnd::PreCreateWindow(cs) )
+		return FALSE;
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	cs.dwExStyle &= ~WS_EX_CLIENTEDGE;
+	cs.lpszClass = AfxRegisterWndClass(0);
+	return TRUE;
+}
+
+// CMainFrame 진단
+
+#ifdef _DEBUG
+void CMainFrame::AssertValid() const
+{
+	CFrameWnd::AssertValid();
+}
+
+void CMainFrame::Dump(CDumpContext& dc) const
+{
+	CFrameWnd::Dump(dc);
+}
+#endif //_DEBUG
+
+
+// CMainFrame 메시지 처리기
+
+void CMainFrame::OnSetFocus(CWnd* /*pOldWnd*/)
+{
+	// 뷰 창으로 포커스를 이동합니다.
+	m_wndView.SetFocus();
+}
+
+BOOL CMainFrame::OnCmdMsg(UINT nID, int nCode, void* pExtra, AFX_CMDHANDLERINFO* pHandlerInfo)
+{
+	// 뷰에서 첫째 크랙이 해당 명령에 나타나도록 합니다.
+	if (m_wndView.OnCmdMsg(nID, nCode, pExtra, pHandlerInfo))
+		return TRUE;
+
+	// 그렇지 않으면 기본 처리합니다.
+	return CFrameWnd::OnCmdMsg(nID, nCode, pExtra, pHandlerInfo);
+}
+
+
+
+void CMainFrame::OnNcPaint()
+{
+	// TODO: 여기에 메시지 처리기 코드를 추가합니다.
+	// 그리기 메시지에 대해서는 CFrameWnd::OnNcPaint()을(를) 호출하지 마십시오.
+	CWindowDC dc(this);
+	int height = ::GetSystemMetrics(SM_CYSIZE) + GetSystemMetrics(SM_CYSIZEFRAME);
+	dc.Rectangle(150, 0, 350, height + 1);
+
+	CRect rect;
+	GetWindowRect(&rect);
+
+	CString str;
+	str.Format(_T("Width = %4d, Height = %4d"), rect.Width(), rect.Height());
+	dc.TextOut(160, 7, str);
+
+	//상위단계로 불려오면 프로그램이 끝남
+	//CFrameWnd::OnNcPaint();
+	//안그려야함 
+}
